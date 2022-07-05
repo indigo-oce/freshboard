b39758b5f07a59385be384bcf50264a33d37243e
indigo-oce <indigoschulze@gmail.com>
2022-07-06 02:28:31 +0800

    ENH: + 'No Projects Yet...' user message

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,12 +2,14 @@
 <html>
 <h1>Birdboard</h1>
 
-@foreach ($projects as $project)
+@forelse ($projects as $project)
 <article>
     <h1> <a href="{{ $project->path() }}">{{ $project->title }}</a> </h1>
     <p> {{ $project->description }} </p>
 
 </article>
-@endforeach
+@empty
+<p>No Projects Yet...</p>
+@endforelse
 
 </html>
