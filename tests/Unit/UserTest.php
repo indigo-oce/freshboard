b0eef359c0cfb2ed7e928ede55ecda84eb8f0bf7
indigo-oce <indigoschulze@gmail.com>
2022-07-07 20:51:00 +0800

    TST! + test_a_user_has_projects()

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,17 +2,19 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    use RefreshDatabase;
+
+    public function test_a_user_has_projects()
     {
-        $this->assertTrue(true);
+        $user = User::factory()->create();
+
+        $this->assertInstanceOf(Collection::class, $user->projects);
     }
 }
