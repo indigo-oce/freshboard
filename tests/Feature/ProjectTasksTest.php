47585cd7d88da94642d98c47faf76e3813ebbb47
indigo-oce <indigoschulze@gmail.com>
2022-07-09 17:56:30 +0800

    TST! test guests & non-owners cannot add tasks

--- tests/Feature/ProjectTasksTest.php
+++ tests/Feature/ProjectTasksTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Models\Task;
+use App\Models\User;
 use App\Models\Project;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -11,6 +12,32 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class ProjectTasksTest extends TestCase
 {
     use RefreshDatabase;
+
+    public function test_guests_cannot_add_tasks_to_a_project()
+    {
+        $project = Project::factory()->create();
+
+        $attributes = Task::factory()->raw();
+
+        $this->post($project->path() . '/tasks', $attributes)
+            ->assertRedirect('login');
+    }
+
+    public function test_a_user_cannot_add_tasks_to_the_projects_of_others()
+    {
+        $this->signIn();
+
+        $project = Project::factory()->create([
+            'owner_id' => User::factory()->create()
+        ]);
+
+        $attributes = Task::factory()->raw();
+
+        $this->post($project->path() . '/tasks', $attributes)
+            ->assertStatus(403);
+
+        $this->assertDatabaseMissing('tasks', $attributes);
+    }
 
     public function test_a_project_can_have_tasks()
     {
